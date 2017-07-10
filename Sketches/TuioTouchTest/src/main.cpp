b5781f9e7cab9e43f0c419b6c8ac62b7614aad2c
Oriol Ferrer Mesia <oriol@uri.cat>
2017-07-10 12:18:40 -0400

    add Touch Test sketch - osx only for now

---
+++ Sketches/TuioTouchTest/src/main.cpp
@@ -0,0 +1,20 @@
+#include "ofMain.h"
+#include "testApp.h"
+//========================================================================
+int main( ){
+
+    ofGLFWWindowSettings winSettings;
+    winSettings.numSamples = 0;
+    winSettings.width = 1280;
+    winSettings.height = 720;
+
+    shared_ptr<ofAppBaseWindow> win = ofCreateWindow(winSettings);	// sets up the opengl context!
+    //((ofAppGLFWWindow*)win.get())->setMultiDisplayFullscreen(true);
+    
+    //ofSetupOpenGL(&win, w, h, OF_WINDOW /*OF_FULLSCREEN*/);	// <-------- setup the GL context
+    
+    ofRunApp(win, shared_ptr<ofBaseApp>(new testApp()));
+    ofSetFullscreen(true);
+    ofRunMainLoop();
+
+}
