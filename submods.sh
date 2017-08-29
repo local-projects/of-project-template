1474d3270d6453b858682d0702ee53cc916ef82f
Oriol Ferrer Mesia <oriol@uri.cat>
2017-08-29 18:01:10 -0400

    update ofxRemoteUI with websockets in ofxApp example

--- submods.sh
+++ submods.sh
@@ -85,6 +85,7 @@ SubModule: ExternalAddons/ofxTuio https://github.com/local-projects/ofxTuio.git 
 SubModule: ExternalAddons/ofxUserContentUpload https://github.com/local-projects/ofxUserContentUpload.git armadillu?https://github.com/armadillu/ofxUserContentUpload.git
 SubModule: ExternalAddons/ofxNanoVG https://github.com/local-projects/ofxNanoVG.git armadillu?https://github.com/armadillu/ofxNanoVG.git galsasson?https://github.com/galsasson/ofxNanoVG.git
 SubModule: ExternalAddons/ofxImageResizer https://github.com/local-projects/ofxImageResizer.git armadillu?https://github.com/armadillu/ofxImageResizer.git
+SubModule: ExternalAddons/ofxLibwebsockets https://github.com/local-projects/ofxLibwebsockets.git upstream?https://github.com/robotconscience/ofxLibwebsockets.git
 
 SubModule: OpenFrameworks https://github.com/local-projects/OpenFrameworks.git OF?https://github.com/OpenFrameworks/OpenFrameworks.git
 }
